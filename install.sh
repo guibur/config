54344b9acf1d92fdf6ad188edcaeb216869a63d5
guillaumeb <guillaume.burger@wandercraft.eu>
2019-08-21 19:17:56 +0200

    Update

--- install.sh
+++ install.sh
@@ -1,1 +1,17 @@
-ln -s $HOME/Private/config/nvim_configs/nvim $HOME/.config/nvim
+ln -s $HOME/Private/config/nvim_configs/nvim $HOME/.config
+
+cd config_files
+for f in *
+do
+    ln -s $HOME/Private/config/config_files/$f $HOME/.config/$f
+done
+cd -
+
+cd dot_files
+for f in *
+do
+    ln -s $HOME/Private/config/dot_files/$f $HOME/${f/dot_/.}
+done
+cd -
+
+lesskey lesskey_description
