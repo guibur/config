4dfcbfd129a807d55b0a5b26da543b8bdc131096
guillaumeb <guillaume.burger@wandercraft.eu>
2020-01-10 12:46:50 +0100

    Update installation script to use recommended installation of neovim

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 ln -s $HOME/Private/config/nvim_configs/nvim $HOME/.config
-pip3 install neovim
+
+# Install neovim and dependencies
+mkdir -p $HOME/.soft_manually_installed
+cd $HOME/.soft_manually_installed
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+chmod u+x nvim.appimage
+ln -s $HOME/.soft_manually_installed/nvim.appimage $HOME/.local/bin/nvim
+pip3 install -U pynvim neovim-remote --user
+pip2 install -U pynvim --user
+cd -
+
+cd nvim_configs
+ln -s $HOME/Private/config/nvim $HOME/.config/nvim
+nvim -c "PlugUpgrade | PlugInstall | PlugUpdate | UpdateRemotePlugins"
+cd -
 
 cd config_files
 for f in *
